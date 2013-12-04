db785e807cb379dc502e5e964a7a0817098ab135
andyedinborough <andy@edinborough.org>
2013-12-03 22:30:03 -0600

    update nuget

--- Create-Package.ps1
+++ Create-Package.ps1
@@ -1,23 +1,24 @@
 $name = "XHTMLr";
 function create-nuspec() {    
-    $spec = get-text "XHTMLr.nuspec"
-    $spec = $spec.Replace("#version#", (get-version("bin\release\$name.dll")))
-    $spec = $spec.Replace("#message#", (get-text(".git\COMMIT_EDITMSG")))
-    
-    $spec | out-file "bin\Package\$name.nuspec"
+		$spec = get-text "XHTMLr.nuspec"
+		$spec = $spec.Replace("#version#", (get-version("bin\release\$name.dll")))
+		$spec = $spec.Replace("#message#", (get-text(".git\COMMIT_EDITMSG")))
+		
+		$spec | out-file "bin\Package\$name.nuspec"
 }
 
 function get-text($file) {
-    return [string]::join([environment]::newline, (get-content -path $file))
+		return [string]::join([environment]::newline, (get-content -path $file))
 }
 
 function get-version($file) {
-    $file = [system.io.path]::combine([environment]::currentdirectory, $file)
-    return [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file).FileVersion
+		$ANOTHERONE = resolve-path .
+		$file = (join-path "$ANOTHERONE" "$file")
+		return [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file).FileVersion
 }
 
 del "bin\Package" -recurse
 md "bin\Package\lib\net40" 
-copy "bin\Release\*.*" "bin\Package\lib\net40"
+copy "bin\Release\*.*" "bin\Package\lib\net45"
 create-nuspec
 .nuget\NuGet.exe pack "bin\Package\$name.nuspec" /o "bin\Package"
